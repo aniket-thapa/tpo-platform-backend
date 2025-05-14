0b57d0491808540d22d1a39af28e4c2813297566
Aniket Thapa <aniketthapa04@gmail.com>
2025-05-15 00:41:20 +0530

    fixed error handling

--- models/User.js
+++ models/User.js
@@ -5,7 +5,10 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    rollno: { type: String, unique: true, sparse: true },
+    rollno: {
+      type: String,
+      required: true,
+    },
     role: { type: String, enum: ['student', 'admin'], default: 'student' },
     password: { type: String, required: true },
   },
